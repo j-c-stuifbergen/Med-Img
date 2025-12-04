dfa2b4b9a960182f8e5544f5f8a6b045e9af75fc
Nathan van Himbergen <n.w.vanhimbergen@student.tudelft.nl>
2025-12-04 11:24:33 +0100

    Entered files and datapath

--- Task1.py
+++ Task1.py
@@ -2,4 +2,5 @@ import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
-water = loadmat('hist_140.mat')
+water = loadmat('data\dataset\task_1\water\hist_140.mat')
+fat = loadmat('data\dataset\task_1\fat\hist_140.mat')
